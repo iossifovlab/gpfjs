a79bb1ff51b5d6d0fdba0866bdca576900c8d949
joankosev <joankosev@gmail.com>
2024-07-18 16:37:08 +0300

    downgrade ngxs version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -134,7 +134,7 @@ import { FamilyTypeFilterState } from './family-type-filter/family-type-filter.s
 import { SortingButtonsComponent } from './sorting-buttons/sorting-buttons.component';
 import { BnNgIdleService } from 'bn-ng-idle';
 import { NgxsModule } from '@ngxs/store';
-import { withNgxsResetPlugin } from 'ngxs-reset-plugin';
+import { NgxsResetPluginModule } from 'ngxs-reset-plugin';
 import { VarianttypesState } from './variant-types/variant-types.state';
 import { EffecttypesState } from './effect-types/effect-types.state';
 import { GenderState } from './gender/gender.state';
@@ -434,6 +434,7 @@ const appRoutes: Routes = [
       UniqueFamilyVariantsFilterState, ErrorsState, GeneProfilesState, DatasetNodeState, DatasetState
     ], {compatibility: { strictContentSecurityPolicy: true }}
     ),
+    NgxsResetPluginModule,
     DragDropModule,
     ClipboardModule,
     ScrollingModule,
@@ -494,7 +495,6 @@ const appRoutes: Routes = [
       deps: [Sentry.TraceService],
       multi: true,
     },
-    withNgxsResetPlugin()
   ],
 
   bootstrap: [AppComponent]
